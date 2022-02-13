7070bd5bccd465d8fa7f6f2e124ffd0b7fcf9b3e
Vitor Emidio <vitorsemidio.96@gmail.com>
2022-02-13 13:53:22 -0300

    Aula 01 completa Squashed commit of the following:
    commit c43c7070af303f8ded4c8a049d454c7ed48a2594
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:51:59 2022 -0300
    
        Adicionado coverage ao gitignore
    
    commit fc6b96df006a6fbbc7be55929fdf002c244479db
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:51:36 2022 -0300
    
        Adição types/jest ao tsconfig
    
    commit 031ab2683e515716e9fa823ce46c17dddf4daf62
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:45:17 2022 -0300
    
        Atualização dependências nuxtjs e tailwind
    
    commit 0a5a1425024b6384a05ff8eb904b4ba837571359
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:27:49 2022 -0300
    
        Configs eslint, jest, nuxt, tawindows e vue
    
    commit acf10ae56749b8ae8cc1ab55c406bdb71606d46c
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:27:27 2022 -0300
    
        Lint pages/layouts
    
    commit e379532b6c4846aa83a75d254aa87e7a9169fe0a
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:27:09 2022 -0300
    
        Criação assets css
    
    commit 039d859b78275981ebaae4d7691461097933f930
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:13:47 2022 -0300
    
        Adição dependências
    
        cookie, types jest, postcss, tawilwind
    
    commit 3880631c0e7d37d01f2d03ed6d53d5a322f961e3
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:09:18 2022 -0300
    
        Alteração scripts de lint e test
    
    commit fd16cdc1e68bd5a2e50318aa5925f90c3382d2fc
    Author: Vitor Emidio <vitorsemidio.96@gmail.com>
    Date:   Sun Feb 13 13:06:53 2022 -0300
    
        Adição assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <Tutorial />
+	<div>vazio</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
-export default Vue.extend({
-  name: 'IndexPage'
-})
+export default Vue.extend({});
 </script>
